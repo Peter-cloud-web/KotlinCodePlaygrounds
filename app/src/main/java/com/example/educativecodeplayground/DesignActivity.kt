cb658fa44b90189a89f523c358b025e95cce0984
Peter-cloud-web <petergichia165@gmail.com>
2023-10-22 23:16:12 +0300

    Add constraints to the added views

---
+++ app/src/main/java/com/example/educativecodeplayground/DesignActivity.kt
@@ -0,0 +1,11 @@
+package com.example.educativecodeplayground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DesignActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_design)
+    }
+}
