49530542e1d18b24b59f78116f341913a5926783
Peter-cloud-web <petergichia165@gmail.com>
2023-10-26 11:48:02 +0300

    Add constraints

--- app/src/main/java/com/example/educativecodeplayground/MainActivity.kt
+++ app/src/main/java/com/example/educativecodeplayground/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.educativecodeplayground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.Delay
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
